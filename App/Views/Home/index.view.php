f7d1268f0afe36e86b3f6103f47994bacec7ac71
Stick <sompilarcikmichal@gmail.com>
2025-11-14 16:08:48 +0100

    fix: cesta do css style lebo root je v public

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -4,14 +4,12 @@
 ?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <title>GymApp | Domov</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
-<main class="container py-5">
+<main class="vitaj">
     <h1 class="text-center mb-4">Vitajte v našej posilňovni!</h1>
     <p class="lead text-center">Spravujte tréningy, permanentky a rezervácie jednoducho online.</p>
     <div class="text-center mt-4">
--- App/Views/Layouts/root.layout.view.php
+++ App/Views/Layouts/root.layout.view.php
@@ -10,18 +10,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Bronze Gym - Template</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/public/css/style.css">
-    <style>
-        .brand-icon {
-            width: 28px;
-            height: 28px;
-        }
-        /* Reverse order on the left side now */
-        .left-group {
-            flex-direction: row-reverse;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/style.css">
+
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-light border-bottom py-2">
