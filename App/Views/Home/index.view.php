7fdc04c25001fa3cb0db009813a5342c6bdda906
Stick <sompilarcikmichal@gmail.com>
2025-11-15 19:28:21 +0100

    home-page hotova, este nie je flexibilna zmenam v obrazhovke a mensim obrazovkam

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -59,7 +59,35 @@
 
 <div class="otvaracie-hodiny-container">
     <div class="otvaracie-hodiny-image">
-        <img src="/images/bikes.jpg" alt="Posilňovňa" />
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <!-- Čiary -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- Obrázky -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/images/home-slide-1.jpg" class="d-block w-100" alt="Obrázok 1">
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/home-slide-2.jpg" class="d-block w-100" alt="Obrázok 2">
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/home-slide-3.jpg" class="d-block w-100" alt="Obrázok 3">
+                </div>
+            </div>
+
+            <!-- Šípky -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
     </div>
     <div class="otvaracie-hodiny-text">
         <h2>Otváracie hodiny:</h2>
